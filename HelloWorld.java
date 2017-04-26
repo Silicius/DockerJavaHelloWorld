64b2c6bdb6c1e33be40d83319f77d3f5380c8b88
root <root@node5.localdomain>
2017-04-26 13:55:44 +0000

    Fix arraybounds

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld{
 	public static void main (String [] args){
 		System.out.println("hello world");
 		if (args.length > 0)
-			for (int i = 0; i<=args.length; i++)	 
+			for (int i = 0; i<args.length; i++)	 
 				System.out.println(args[i]);
 	}
 }
