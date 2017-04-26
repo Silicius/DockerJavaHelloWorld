d52e1797da0e1d03f981aee9a37be1900ae06416
root <root@node5.localdomain>
2017-04-26 13:45:42 +0000

    print args

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld{
 	public static void main (String [] args){
 		System.out.println("hello world");
-		if (args.length > 0) 
-			System.out.println(args[0]);
+		if (args.length > 0)
+			for (int i = 0; i<=args.length; i++)	 
+				System.out.println(args[i]);
 	}
 }
